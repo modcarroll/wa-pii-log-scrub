75e3540248465ffcdfc4dff6e751d9bad6f5d678
modlanglais <morgandlanglais@gmail.com>
2020-11-18 16:54:30 -0600

    incorporates scrubadub

--- main.py
+++ main.py
@@ -5,33 +5,19 @@ inputFile = open('data/morgantownlogs.json')
 jsonText = json.load(inputFile)
 
 for item in jsonText:
-    print()
-    print(json.dumps(item, indent=4))
-    print()
+    item['request']['context']['integrations'] = "REDACTED"
+    item['request']['input']['text'] = scrubadub.clean(json.dumps(item['request']['input']['text']), replace_with='placeholder')
+    item['response']['input']['text'] = scrubadub.clean(json.dumps(item['response']['input']['text']), replace_with='placeholder')
+    item['response']['context']['integrations'] = "REDACTED"
 
-inputFile.close()
+    for output in item['response']['output']['generic']:
+        if output["response_type"] == "text":
+            output['text'] = scrubadub.clean(json.dumps(output['text']), replace_with='placeholder')
 
-# jsonText.forEach(function (item, index) {
-#   console.log("---------------------");
-#   console.log(item);
-#
-#   # need to handle both input and output in item.request and item.response
-#
-#   const redactedInput = redactor.redact(item.input.text);
-#   item.input.text = redactedInput;
-#
-#   # will need output.text and output.generic.text
-#
-#   const redactedOutput = redactor.redact(item.output.generic.text);
-#   item.output.generic.text = redactedOutput;
-#
-# // let jsonString = JSON.stringify(jsonText);
-#
-#
-# // make a loop
-# // for each input do the below method
-# // then take the new string and do a .replace(input, redactedText)
-#
-# // const redactedText = redactor.redact(jsonString);
-#
-# // console.log(redactedText);
+    for texts in item['response']['output']['text']:
+        texts = scrubadub.clean(texts, replace_with='placeholder')
+
+    item['response']['input']['text'] = scrubadub.clean(json.dumps(item['response']['input']['text']), replace_with='placeholder')
+
+with open("data/morgantownlogs.json", "w") as inputFile:
+    json.dump(jsonText, inputFile)
